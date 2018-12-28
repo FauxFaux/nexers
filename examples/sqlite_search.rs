71c8d7566487833ef808e3090fcb8c4b241d1647
Chris West (Faux) <git@goeswhere.com>
2018-12-28 23:13:30 +0000

    write in a different thread

--- examples/sqlite_search.rs
+++ examples/sqlite_search.rs
@@ -1,25 +1,26 @@
+use std::fs;
 use std::io;
+use std::mem;
+use std::sync::mpsc;
+use std::thread;
 
+use failure::format_err;
 use failure::Error;
 
+use nexers::Doc;
 use nexers::Event;
 
 fn main() -> Result<(), Error> {
-    use std::fs;
     let from = io::BufReader::new(fs::File::open("sample-index")?);
     let mut errors = 0;
-    let mut sql = rusqlite::Connection::open("search.db")?;
-    sql.execute_batch(include_str!("../schema.sql"))?;
-    let tran = sql.transaction()?;
-    let mut db = nexers::sqlite::Db::new(tran)?;
-    let mut pos = 0usize;
+
+    let (send, recv) = mpsc::sync_channel(1_024);
+
+    let writer = write(recv);
+
     nexers::read(from, |event| {
-        pos += 1;
-        if 0 == pos % 10000 {
-            println!("{}", pos);
-        }
         match event {
-            Event::Doc(d) => db.add(&d)?,
+            Event::Doc(d) => send.send(d)?,
 
             Event::Error { .. } => errors += 1,
             Event::Delete(_) => (),
@@ -27,10 +28,37 @@ fn main() -> Result<(), Error> {
         Ok(())
     })?;
 
-    db.commit()?;
+    mem::drop(send);
+
+    writer.join().map_err(|e| format_err!("panic: {:?}", e))??;
+
     println!("..and {} errors", errors);
 
     //    println!("{:?}", db.find_versions("com.google.guava", "guava")?);
 
     Ok(())
+}
+
+fn write(recv: mpsc::Receiver<Doc>) -> thread::JoinHandle<Result<(), Error>> {
+    thread::spawn(move || -> Result<(), Error> {
+        let mut sql = rusqlite::Connection::open("search.db")?;
+        sql.execute_batch(include_str!("../schema.sql"))?;
+        let tran = sql.transaction()?;
+        let mut db = nexers::sqlite::Db::new(tran)?;
+
+        let mut pos = 0usize;
+
+        while let Some(doc) = recv.recv().ok() {
+            pos += 1;
+            if 0 == pos % 10000 {
+                println!("{}", pos);
+            }
+
+            db.add(&doc)?;
+        }
+
+        db.commit()?;
+
+        Ok(())
+    })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod pivot;
 pub mod sqlite;
 
 pub use self::nexus::read;
+pub use self::nexus::Doc;
 pub use self::nexus::Event;
 
 pub use self::pivot::Db;
