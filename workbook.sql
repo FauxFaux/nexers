39860e130eefc4593da4635352c3ba3a044d23c9
Chris West (Faux) <git@goeswhere.com>
2019-09-22 14:20:10 +0100

    workbook += newest upload

--- workbook.sql
+++ workbook.sql
@@ -15,3 +15,18 @@ select id,
        name_id,
        desc_id
 from versions;
+
+create index versions_last_modified on versions (last_modified);
+create index versions_ga on versions (group_id, artifact_id);
+
+-- newest upload of each group/artifact
+select (select name from group_names where id = group_id)       as g,
+       (select name from artifact_names where id = artifact_id) as a,
+       (select version
+        from versions v
+        where v.group_id = p.group_id
+          and v.artifact_id = p.artifact_id
+        order by last_modified desc
+        limit 1)                                                as v
+from (select distinct group_id, artifact_id from versions) p
+order by g, a;
